13ad155f10326c862aef085d473eaacdeb39c17d
Evangelos Papakirykou <vpapakir@gmail.com>
2025-12-31 21:19:41 +0200

    [github] [azure] [ado] [apply] fix: remove cloud configuration for this module, i reckon it inherits from the root module

--- backend.tf
+++ backend.tf
@@ -1,12 +1,5 @@
 terraform {
   required_version = ">= 1.14.0"
-  cloud {
-    organization = "vpapakir"
-
-    workspaces {
-      name = "azure-infra-dev"
-    }
-  }
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
