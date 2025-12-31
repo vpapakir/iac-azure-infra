46af87c639553989a8bebba46c3922ab41238d10
Evangelos Papakirykou <vpapakir@gmail.com>
2025-12-31 19:43:09 +0200

    [github] [azure] [ado] fix: import 2 existing rgs

--- outputs.tf
+++ outputs.tf
@@ -1,29 +1,9 @@
-output "resource_group_gen_id" {
-  description = "ID of the created resource group"
-  value       = module.resourcegroup_gen.id
-}
-
-output "resource_group_gen_name" {
-  description = "Name of the created resource group"
-  value       = module.resourcegroup_gen.name
-}
-
 output "resource_group_gen_location" {
   description = "Location of the created resource group"
   value       = module.resourcegroup_gen.location
 }
 
-output "resource_group_datalake_id" {
-  description = "ID of the created resource group"
-  value       = module.resourcegroup_gen.id
-}
-
-output "resource_group_datalake_name" {
-  description = "Name of the created resource group"
-  value       = module.resourcegroup_gen.name
-}
-
 output "resource_group_datalake_location" {
   description = "Location of the created resource group"
-  value       = module.resourcegroup_gen.location
+  value       = module.resourcegroup_datalake.location
 }
