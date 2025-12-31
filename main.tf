b41b5b96c3ab67443977abab3dfcc780718d5dde
Evangelos Papakirykou <vpapakir@gmail.com>
2025-12-31 20:11:05 +0200

    [github] [azure] [ado] [apply] fix: import 1 existing rgs

--- main.tf
+++ main.tf
@@ -7,14 +7,14 @@ provider "azurerm" {
 }
 
 # Resource Group using published atom
-# module "resourcegroup_api" {
-#   source  = "app.terraform.io/vpapakir/resourcegroup/atom"
-#   version = "0.0.2"
+module "resourcegroup_api" {
+  source  = "app.terraform.io/vpapakir/resourcegroup/atom"
+  version = "0.0.2"
 
-#   name     = var.resource_group_name_api
-#   location = var.location_api
-#   tags     = var.tags_api
-# }
+  name     = var.resource_group_name_api
+  location = var.location_api
+  tags     = var.tags_api
+}
 
 # Resource Group using published atom
 module "resourcegroup_gen" {
