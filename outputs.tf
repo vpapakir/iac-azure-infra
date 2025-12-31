2dd753e5858fba3abbfb86a8d06ba39dfd2232c3
Evangelos Papakirykou <vpapakir@gmail.com>
2025-12-31 16:04:32 +0200

    [github] [azure] [ado] test: no output vars

--- outputs.tf
+++ outputs.tf
@@ -1,17 +1,17 @@
-output "resource_group_id" {
-  description = "ID of the created resource group"
-  value       = module.resourcegroup.id
-}
+# output "resource_group_id" {
+#   description = "ID of the created resource group"
+#   value       = module.resourcegroup.id
+# }
 
-output "resource_group_name" {
-  description = "Name of the created resource group"
-  value       = module.resourcegroup.name
-}
+# output "resource_group_name" {
+#   description = "Name of the created resource group"
+#   value       = module.resourcegroup.name
+# }
 
-output "resource_group_location" {
-  description = "Location of the created resource group"
-  value       = module.resourcegroup.location
-}
+# output "resource_group_location" {
+#   description = "Location of the created resource group"
+#   value       = module.resourcegroup.location
+# }
 
 # Data Factory outputs (when module is enabled)
 # output "data_factory_id" {
