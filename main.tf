25b2bc3436a1cbc6a4cd4290191c6b9d16176506
Evangelos Papakirykou <vpapakir@gmail.com>
2025-12-31 18:38:15 +0200

    [github] [azure] [ado] fix: bump up reference to rg atom

--- main.tf
+++ main.tf
@@ -8,24 +8,9 @@ provider "azurerm" {
 # Resource Group using published atom
 module "resourcegroup" {
   source  = "app.terraform.io/vpapakir/resourcegroup/atom"
-  version = "0.0.1"
+  version = "0.0.2"
 
   name     = var.resource_group_name
   location = var.location
   tags     = var.tags
 }
-
-# Data Factory using published atom (when available)
-# module "data_factory" {
-#   source  = "app.terraform.io/vpapakir/datafactory/azure"
-#   version = "~> 1.0"
-#   
-#   name                = var.data_factory_name
-#   location            = var.location
-#   resource_group_name = module.resource_group.name
-#   
-#   public_network_enabled          = var.data_factory_public_network_enabled
-#   managed_virtual_network_enabled = var.data_factory_managed_vnet_enabled
-#   
-#   tags = var.tags
-# }
