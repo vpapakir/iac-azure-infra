ba440e78a02306e4e0007b5a37683619fe409de6
Evangelos Papakirykou <vpapakir@gmail.com>
2025-12-31 20:05:18 +0200

    [github] [azure] [ado] fix: import 2 existing rgs

--- outputs.tf
+++ outputs.tf
@@ -3,7 +3,7 @@ output "resource_group_gen_location" {
   value       = module.resourcegroup_gen.location
 }
 
-output "resource_group_api_location" {
-  description = "Location of the created resource group"
-  value       = module.resourcegroup_api.location
-}
+# output "resource_group_api_location" {
+#   description = "Location of the created resource group"
+#   value       = module.resourcegroup_api.location
+# }
