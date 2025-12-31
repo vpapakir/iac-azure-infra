46f6d59d2e0ada8e357ccf4f54b11494bf3b4f2d
Evangelos Papakirykou <vpapakir@gmail.com>
2025-12-31 18:53:55 +0200

    [github] [azure] [ado] fix: import 2 existing rgs

--- variables.tf
+++ variables.tf
@@ -9,23 +9,6 @@ variable "location" {
   default     = "West Europe"
 }
 
-variable "data_factory_name" {
-  description = "Name of the Azure Data Factory"
-  type        = string
-}
-
-variable "data_factory_public_network_enabled" {
-  description = "Enable public network access for Data Factory"
-  type        = bool
-  default     = true
-}
-
-variable "data_factory_managed_vnet_enabled" {
-  description = "Enable managed virtual network for Data Factory"
-  type        = bool
-  default     = true
-}
-
 variable "tags" {
   description = "Tags to apply to all resources"
   type        = map(string)
