efd3b7d2af3175c85095b398cf3577daa4b6cf62
Evangelos Papakirykou <vpapakir@gmail.com>
2025-12-31 20:15:36 +0200

    [github] [azure] [ado] [apply] fix: import 1 existing rgs

--- imports.tf
+++ imports.tf
@@ -1,9 +1,9 @@
-# import {
-#   to = module.resourcegroup_gen.azurerm_resource_group.main
-#   id = "/subscriptions/52ce15a5-9158-46b9-ab49-2b932ed43df9/resourceGroups/rg-weu-dev-gen-001"
-# }
-
 import {
-  to = module.resourcegroup_api.azurerm_resource_group.main
-  id = "/subscriptions/52ce15a5-9158-46b9-ab49-2b932ed43df9/resourceGroups/rg-weu-dev-api-001"
+  to = module.resourcegroup_gen.azurerm_resource_group.main
+  id = "/subscriptions/52ce15a5-9158-46b9-ab49-2b932ed43df9/resourceGroups/rg-weu-dev-gen-001"
 }
+
+# import {
+#   to = module.resourcegroup_api.azurerm_resource_group.main
+#   id = "/subscriptions/52ce15a5-9158-46b9-ab49-2b932ed43df9/resourceGroups/rg-weu-dev-api-001"
+# }
