257f5144884614b5e9a6ccf9805e50dca9639030
Evangelos Papakirykou <vpapakir@gmail.com>
2026-01-03 12:51:31 +0200

    [github] [azure] [ado] [apply] fix: rearrange tf backend configuration so the state persists in the cloud rather than locally

--- backend.tf
+++ backend.tf
@@ -1,9 +1,6 @@
 terraform {
-  required_version = ">= 1.6.0"
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 3.0"
-    }
+  cloud {
+    organization = "vpapakir"
+    # Workspace will be set dynamically via TF_WORKSPACE environment variable
   }
 }
