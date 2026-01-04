546a4a9d8cd9e4a70f45efdb6a6d5e8361bba743
Evangelos Papakirykou <vpapakir@gmail.com>
2026-01-05 01:58:29 +0200

    [github] [azure] [ado] [apply] release: the managed dbx resource group cannot be managed by terraform

--- 10.main-manageddbx.tf
+++ 10.main-manageddbx.tf
@@ -1,17 +1,19 @@
-# Resource Group using published atom (Databricks managed - read-only)
-module "resourcegroup_manageddbx" {
-  source  = "app.terraform.io/vpapakir/resourcegroup/atom"
-  version = "0.0.2"
-
+# Databricks-managed resource group - use direct resource instead of atom
+resource "azurerm_resource_group" "manageddbx" {
   name     = local.resource_groups.manageddbx.name
   location = local.resource_groups.manageddbx.location
-  tags     = {}  # Don't try to manage tags on Databricks-managed RG
-
-  # Prevent Terraform from trying to modify this managed resource
+  
+  # Keep existing Databricks management
+  managed_by = "/subscriptions/52ce15a5-9158-46b9-ab49-2b932ed43df9/resourceGroups/rg-weu-dev-datalake-001/providers/Microsoft.Databricks/workspaces/dbx-weu-dev-datalake-001"
+  
+  tags = {
+    MLWorkspaceLinkUpdateTime = "04/12/2024 08:31:26 +00:00"
+  }
+  
   lifecycle {
     ignore_changes = [
-      tags,
-      managed_by
+      managed_by,
+      tags
     ]
   }
 }
